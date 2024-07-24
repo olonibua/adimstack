6020317daeecac140b71a9e538b5f0b56651421a
olonibua tolulope <olonibua0001@gmail.com>
2024-07-24 16:00:59 +0100

    new chnages

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -30,11 +30,11 @@ Sentry.init({
   
 });
 
-Sentry.init({
-  dsn: "https://a681b022c4a8bb40aad64dc6056e8526@o4507656705933312.ingest.us.sentry.io/4507656717402112",
+// Sentry.init({
+//   dsn: "https://a681b022c4a8bb40aad64dc6056e8526@o4507656705933312.ingest.us.sentry.io/4507656717402112",
 
-  integrations: [Sentry.replayIntegration()],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+//   integrations: [Sentry.replayIntegration()],
+//   // Session Replay
+//   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// });
